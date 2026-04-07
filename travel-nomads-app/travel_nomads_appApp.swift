51c311800c8ead0ab0616c825dfa3fc19453181b
Sumi Sastri <sumisastri@gmail.com>
2026-04-07 22:12:31 +0100

    Initial Commit

---
+++ travel-nomads-app/travel_nomads_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  travel_nomads_appApp.swift
+//  travel-nomads-app
+//
+//  Created by Sumi Sastri on 07/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct travel_nomads_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
